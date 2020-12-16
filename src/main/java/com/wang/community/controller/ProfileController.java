eeb9676780bc60780030ab9dae1cd6296f03a303
wuyi2 <874605322@qq.com>
2020-12-16 16:31:57 +0800

    add profile

--- src/main/java/com/wang/community/controller/ProfileController.java
+++ src/main/java/com/wang/community/controller/ProfileController.java
@@ -8,13 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-public class IndexController {
+public class ProfileController {
 
     @Autowired
     private UserMapper userMapper;
@@ -22,18 +23,20 @@ public class IndexController {
     @Autowired
     private QuestionService questionService;
 
-    @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model,
-                        @RequestParam(name = "page",defaultValue = "1") Integer page,
-                        @RequestParam(name = "size",defaultValue = "7") Integer size
-                        ) {
+    @GetMapping("/profile/{action}")
+    public String profile(HttpServletRequest request,
+                          @PathVariable(name = "action") String action,
+                          @RequestParam(name = "page",defaultValue = "1") Integer page,
+                          @RequestParam(name = "size",defaultValue = "7") Integer size,
+                          Model model) {
+
+        User user = null;
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals("token")) {
                     String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
+                    user = userMapper.findByToken(token);
                     if (user != null) {
                         request.getSession().setAttribute("user", user);
                     }
@@ -41,8 +44,21 @@ public class IndexController {
                 }
             }
         }
-        PaginationDTO pagination = questionService.list(page, size);
-        model.addAttribute("pagination",pagination);
-        return "index";
+        if (user == null) {
+            return "redirect:/";
+        }
+
+        if ("questions".equals(action)) {
+            model.addAttribute("section", "question");
+            model.addAttribute("sectionName", "我的问题");
+        } else if ("replies".equals(action)) {
+            model.addAttribute("section", "replies");
+            model.addAttribute("sectionName", "最新回复");
+        }
+
+        PaginationDTO paginationDTO = questionService.list(user.getId(),page,size);
+        model.addAttribute("pagination",paginationDTO);
+
+        return "profile";
     }
 }
