4c71de54b760a8606e3ff30e649be9d72baacfb8
wuyi2 <874605322@qq.com>
2020-12-16 10:30:59 +0800

    add Page navigation

--- src/main/java/com/wang/community/controller/IndexController.java
+++ src/main/java/com/wang/community/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.wang.community.controller;
 
+import com.wang.community.dto.PaginationDTO;
 import com.wang.community.dto.QuestionDTO;
 import com.wang.community.mapper.UserMapper;
 import com.wang.community.model.User;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -24,7 +26,10 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model) {
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "7") Integer size
+                        ) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
@@ -38,8 +43,8 @@ public class IndexController {
                 }
             }
         }
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page, size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
--- src/main/java/com/wang/community/service/QuestionService.java
+++ src/main/java/com/wang/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.wang.community.service;
 
+import com.wang.community.dto.PaginationDTO;
 import com.wang.community.dto.QuestionDTO;
 import com.wang.community.mapper.QuestionMapper;
 import com.wang.community.mapper.UserMapper;
@@ -21,17 +22,33 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount, page, size);
+
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage() ) {
+            page = paginationDTO.getTotalPage() ;
+        }
+
+        //size*(page-1)
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset, size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
             BeanUtils.copyProperties(question, questionDTO);
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
-
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+
+        return paginationDTO;
     }
 }
