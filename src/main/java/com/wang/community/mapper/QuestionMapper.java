aa897bcc14862e6dd684573ca71fd0f5e0024160
wzb <wzb3422@gmail.com>
2020-12-17 10:13:04 +0800

    fix Re edit function

--- src/main/java/com/wang/community/mapper/QuestionMapper.java
+++ src/main/java/com/wang/community/mapper/QuestionMapper.java
@@ -1,10 +1,7 @@
 package com.wang.community.mapper;
 
 import com.wang.community.model.Question;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -31,4 +28,7 @@ public interface QuestionMapper {
 
     @Select("select * from question where id = #{id}")
     Question getById(@Param(value = "id") Integer id);
+
+    @Update("update question set title = #{title}, description = #{description}, gmt_modified = #{gmtModified}, tag = #{tag} where id = #{id}")
+    void update(Question question);
 }
--- src/main/java/com/wang/community/service/QuestionService.java
+++ src/main/java/com/wang/community/service/QuestionService.java
@@ -107,4 +107,15 @@ public class QuestionService {
 
         return questionDTO;
     }
+
+    public void createOrUpdate(Question question) {
+        if (question.getId() == null) {
+            question.setGmtCreate(System.currentTimeMillis());
+            question.setGmtModified(question.getGmtCreate());
+            questionMapper.create(question);
+        } else {
+            question.setGmtModified(question.getGmtCreate());
+            questionMapper.update(question);
+        }
+    }
 }
